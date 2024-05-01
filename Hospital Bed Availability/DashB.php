986dea65a325f7c7c4b54ee98d262e3cc658148b
anudeep7103 <44546239+anudeep7103@users.noreply.github.com>
2024-05-01 11:14:20 +0530

    Add files via upload

---
+++ Hospital Bed Availability/DashB.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+if(!isset($_SESSION['username']))
+{
+header('location:Login.html');
+}
+$db_host="localhost";
+$db_user="root";
+$db_password="";
+$db_name="dashboard";
+
+$conn=mysqli_connect($db_host,$db_user,$db_password,$db_name);
+if(!$conn)
+{
+    die("Connection failed: ".mysqli_connect_error());
+}
+$result=mysqli_prepare($conn,"SELECT * FROM dashinfo");
+mysqli_stmt_bind_result($result,$id,$rec,$admit,$dec,$beds,$vent);
+
+mysqli_stmt_execute($result);
+mysqli_stmt_fetch($result);
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/Features-Boxed.css">
+</head>
+
+
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark navbar-custom" style="position: fixed;">
+        <div class="container"><a class="navbar-brand" style="font-size:150%;">CITY HOSPITAL</a>
+            <button data-toggle="collapse" class="navbar-toggler" data-target="#navbarResponsive"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="DashB.php">DASHBOARD</a></li>
+                    <li class="nav-item"><a class="nav-link" href="RegisterPatients.html">ADD PATIENT DETAILS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="Logout.php">LOG OUT</a></li>
+                    <form class="form-inline" action="data.php" method="POST">
+                        <input name="filter" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <body >
+     
+    
+    <div class="features-boxed">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center" style="margin-top: 70px;color:black">HOSPITAL DASHBOARD </h2>
+            </div>
+            <div class="row justify-content-center features">
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div style ="background-color: rgb(146, 168, 209);" class="box"><i class="fa fa-heart icon"></i>
+                   
+                        <h3 class="name"> Oxygen Cylinders: <?php echo $rec;?> </h3>
+                      
+                        </div>
+                </div>
+                
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    
+                    <div style ="background-color: rgb(146, 168, 209);" class="box"><i class="fa fa-bed icon"></i>
+                        <h3 class="name"> Beds Available : <?php echo $admit;?> </h3>
+                      
+                    </div>
+                
+                </div>
+           
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div style ="background-color: rgb(146, 168, 209);"class="box"><i class="fa fa-ambulance icon"></i>
+                        <h3 class="name">Remedesivir Injections : <?php echo $dec;?> </h3>
+                    
+                    </div>
+                </div>
+                
+             
+            </div>
+        </div>
+    </div>
+
+</body>
+    <section></section>
+    <section></section>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
